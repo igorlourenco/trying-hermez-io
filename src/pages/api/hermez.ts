1e6b63cd1ec070c8cf585cad89aed8ffde8172f4
Igor Lourenço <igorlourenco.dev@gmail.com>
2022-01-10 10:27:25 -0300

    refactor: remove unused imports

--- src/pages/api/hermez.ts
+++ src/pages/api/hermez.ts
@@ -1,9 +1,5 @@
-import hermez from "@hermeznetwork/hermezjs";
 import { NextApiRequest, NextApiResponse } from "next";
-import { responseSymbol } from "next/dist/server/web/spec-compliant/fetch-event";
 import { initializeHermez } from "../../utils/initialize-hermez";
-
-let hermezInitialization = null;
 
 export default async function Hermez(
   request: NextApiRequest,
